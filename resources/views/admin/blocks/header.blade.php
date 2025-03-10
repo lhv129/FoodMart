275763935e6beaf9535485c70a23bfed170a5076
DESKTOP-MQR0I53\vietlh <lhv2k4@gmail.com>
2025-03-10 17:56:35 +0700

    init sources

---
+++ resources/views/admin/blocks/header.blade.php
@@ -0,0 +1,5 @@
+<nav class="navbar bg-info">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{ route('admin/dashboard') }}">Trang Admin</a>
+    </div>
+</nav>
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+    resolve:{
+        alias:{
+            '@': path.resolve(__dirname,'resources'),
+            '@views': path.resolve(__dirname,'resources/views')
+        }
+    }
+});
